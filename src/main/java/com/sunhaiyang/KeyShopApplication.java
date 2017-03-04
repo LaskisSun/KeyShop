8062ffe09e7ac2b1517e8553bbe1e33a94b269db
LaskiSun <haiyang@LaskiSundeMacBook-Air.local>
2017-03-05 01:32:55 +0800

    创建项目KeyShop

---
+++ src/main/java/com/sunhaiyang/KeyShopApplication.java
@@ -0,0 +1,12 @@
+package com.sunhaiyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyShopApplication.class, args);
+	}
+}
